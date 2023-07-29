2bf1fae8e8bb06e99347b60c7cddd4777e1ef662
Wesley Kaihara <wesleykaihara@gmail.com>
2023-07-29 19:50:55 -0300

    Restructuring of Tests

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,12 @@
 class UserService {
   constructor(userRepository, { name, email, password } ) {
-    name: Number
     this.userRepository = userRepository
     this.name = name
     this.email = email
     this.password = password
+    if (!this.validateBody()) {
+      throw new Error("Email, Full name, and Password are required");
+    }
   }
 
   isSafePassword() {
@@ -17,15 +19,26 @@ class UserService {
     return await this.userRepository.find(this.email,"email")?false:true
   }
 
-  avaibleName() {
-    return this.userRepository.find(this.name,"name")?false:true
+  async avaibleName() {
+    return await this.userRepository.find(this.name,"name")?false:true
   }
   
+  validateBody() {
+    if (!this.name || !this.email || !this.password) {
+      throw new Error("Name, Email, and Password are required");
+    }
+    return true
+  }
+
   async save() {
+    if (!this.isSafePassword()) {
+      throw new Error("Password is not strong enough");
+    }
+    
     const isAvaibleEmail = await this.avaibleEmail()
     if(!isAvaibleEmail) throw new Error(`Email "${this.email}" is not avaible`)
 
-    const isAvaibleName = this.avaibleName()
+    const isAvaibleName = await this.avaibleName()
     if(!isAvaibleName) throw new Error(`Name "${this.name}" is not avaible`)
 
     return {
--- tests/unitTests/userService.test.js
+++ tests/unitTests/userService.test.js
@@ -1,56 +1,88 @@
-const { describe, it } = require("mocha")
-const { expect, use } = require("chai")
+const { describe, it } = require("mocha");
+const { expect, use } = require("chai");
 const chaiAsPromised = require("chai-as-promised");
-use(chaiAsPromised)
+use(chaiAsPromised);
 
-const UserService = require('../../src/services/userService')
-const UserRepository = require('../../src/repository/userRepository')
+const UserService = require("../../src/services/userService");
+const UserRepository = require("../../src/repository/userRepository");
 
 const mocks = {
   VALID_USER: require("../mocks/user/valid-user.json"),
+  INVALID_USER: require("../mocks/user/invalid-user-body.json"),
   USER_LIST: require("../mocks/user/user-list.json"),
   USER_WITH_REPETED_NAME: require("../mocks/user/repeted-user-name.json"),
   USER_WITH_REPETED_EMAIL: require("../mocks/user/repeted-user-email.json"),
   UNSAFE_PASSWORD: require("../mocks/user/user-with-unsafe-password.json"),
-}
+};
 
-describe("Users Suite Test",() => {
-  it("should receive email, Full name and password Information")
+describe("Users Suite Test", () => {
+  it("should throw an exception if email, full name or password attributes are not provided", async () => {
+    const userWithInvalidBody = mocks.INVALID_USER;
+    const createUserServiceWithInvalidBody = () =>
+      new UserService([], userWithInvalidBody);
 
-  it("should't allow the registration of two users with the same email", async() => {
-    const userRepository = new UserRepository(mocks.USER_LIST)
-    const userWithRepetedEmail = mocks.USER_WITH_REPETED_EMAIL
-    
-    const userService = new UserService(userRepository, userWithRepetedEmail);
+    expect(createUserServiceWithInvalidBody).to.throw(
+      "Name, Email, and Password are required"
+    );
+  });
 
-    const result = async() => await userService.save()
+  it("should throw an exception when attempting to register a user with an existing email", async () => {
+    const userRepository = new UserRepository(mocks.USER_LIST);
+    const userWithRepeatedEmail = mocks.USER_WITH_REPETED_EMAIL;
+  
+    const userService = new UserService(userRepository, userWithRepeatedEmail);
+  
+    try {
+      await userService.save();
+    } catch (error) {
+      expect(error.message).to.equal(
+        `Email "${userWithRepeatedEmail.email}" is not avaible`
+      );
+    }
+  });
 
-    await expect(result())
-      .to
-      .be
-      .rejectedWith(`Email "${userWithRepetedEmail.email}" is not avaible`)
-  })
+  it("should throw an exception when attempting to register a user with an existing name", async () => {
+    const userRepository = new UserRepository(mocks.USER_LIST);
+    const userWithRepetedName = mocks.USER_WITH_REPETED_NAME;
 
-  it("should't allow the registration of two users with the same full name", async() => {
-    const userRepository = new UserRepository(mocks.USER_LIST)
-    const userWithRepetedName = mocks.USER_WITH_REPETED_NAME
-    
-    const userService = new UserService(userRepository, userWithRepetedName)
-    const result = async() => await userService.save()
+    const userService = new UserService(userRepository, userWithRepetedName);
+      
+    try {
+      await userService.save();
+    } catch (error) {
+      expect(error.message).to.equal(
+        `Name "${userWithRepetedName.name}" is not avaible`
+      );
+    }
+  });
 
-    await expect(result())
-      .to
-      .be
-      .rejectedWith(`Name "${userWithRepetedName.name}" is not avaible`)
-  })
+  it("should throw an exception when password is not strong enough", async () => {
+    const userUnsafePassword = mocks.UNSAFE_PASSWORD;
+  
+    const userService = new UserService([], userUnsafePassword);
 
-  it("should be return false when password is unsafe", () => {
-    const userService = new UserService([], mocks.UNSAFE_PASSWORD);
     expect(userService.isSafePassword()).not.be.true;
-  })
 
-  it("should be return true when password is safe", () => {
-    const userService = new UserService([], mocks.VALID_USER);
+    try {
+      await userService.save();
+    } catch (error) {
+      expect(error.message).to.equal(
+        `Password is not strong enough`
+      );
+    }
+  });
+
+  it("should not throw an exception when password is safe", async() => {
+    const userRepository = new UserRepository([]);
+    const userService = new UserService(userRepository, mocks.VALID_USER);
+
     expect(userService.isSafePassword()).be.true;
-  })
-})
+
+    try {
+      await userService.save();
+    } catch (error) {
+      console.log("error",error);
+    }
+
+  });
+});
