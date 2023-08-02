1203b337a74cda04a0a8f355df98a271f83d3a05
Wesley Kaihara <wesleykaihara@gmail.com>
2023-08-02 18:54:53 -0300

    superagent => supertest

--- tests/e2e/hello-world.test.js
+++ tests/e2e/hello-world.test.js
@@ -1,7 +1,8 @@
 const { describe, it } = require("mocha");
 const { expect } = require("chai");
 
-const superagent = require('superagent');
+const supertest = require('supertest');
+
 const server = require("../../src/server");
 
 describe("#Server", () => {
@@ -11,26 +12,20 @@ describe("#Server", () => {
     app = server(3000);
   });
 
-  after(function() {
+  after(() => {
     app.close();
   });
   
-  it("should be return status 200 in base url" , () => {
-    superagent 
-      .get("http://localhost:3000/")
-      .end((err,res) => {
-        expect(res.statusCode)
-          .to.be.equal(200)
-      })
+  it("should be return status 200 in base url" , (done) => {
+    supertest(app) 
+      .get("/")
+      .expect(200, done)
   })
 
-  it("should be return a value in body" , () => {
-    superagent 
-      .get("http://localhost:3000/")
-      .end((err,res) => {
-        expect(res.body)
-          .to.be.deep.equal({ message: 'Hello World' })
-      })
+  it("should be return a value in body" , (done) => {
+    supertest(app) 
+      .get("/")
+      .expect({message: "Hello World"}, done)
   })
 
 })
